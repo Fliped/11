483e7b639ab8dc58d69fa7c1c239d453a4eebd85
xuyang <wang22958@126.com>
2022-02-24 16:46:07 +0800

    enter

--- Master.py
+++ Master.py
@@ -3,8 +3,9 @@ import logging
 import modbus_tk 
 import modbus_tk.defines as cst
 import modbus_tk.modbus_tcp as modbus_tcp
+import xlwt
 
-modbus_ip="192.168.10.68"
+modbus_ip="0.0.0.0"
 Modbus_port="502"
 
 LOGGER=modbus_tk.utils.create_logger("console")
@@ -12,4 +13,4 @@ MASTER=modbus_tcp.TcpMaster(host=modbus_ip,port=Modbus_port)
 MASTER.set_timeout(5.0)
 LOGGER.info("connected")
 
-LOGGER.info(MASTER.execute(1,cst.READ_HOLDING_REGISTERS,0,4))
+LOGGER.info(MASTER.execute(1,cst.READ_HOLDING_REGISTERS,0,4)) #an integer is required
--- Salve.py
+++ Salve.py
@@ -1,3 +1,5 @@
+
+import imp
 import sys
 import time
 import logging
@@ -6,21 +8,49 @@ import modbus_tk
 import modbus_tk.defines as cst
 import modbus_tk.modbus_tcp as modbus_tcp
 import random
+import xlwt
+import xlrd
 
 def main():
     LOGGER=modbus_tk.utils.create_logger(name="console",record_format="%(message)s")
     # creat server
     try:
-        SERVER=modbus_tcp.TcpServer(address="192.168.1.111",port=502) #server error: [Errno 10049] The requested address is not valid in its context
+        SERVER=modbus_tcp.TcpServer(address="0.0.0.0",port=502) #server error: [Errno 10049] The requested address is not valid in its context
         LOGGER.info("running...")
         LOGGER.info("enter'quit'for closing the server") 
         SERVER.start()
      # creat slave
-        SERVE1=SERVER.add_slave(1)
-        SERVE1.add_block('A',cst.HOLDING_REGISTERS,0,4)   
+        SLAVE1=SERVER.add_slave(1)
+        SLAVE1.add_block('A',cst.HOLDING_REGISTERS,0,2000) 
+        #获取excel的数据   设置在寄存器 
+        SLAVE1.set_values('A',0,getdata())
+       
+
+        while True:
+            CMD=sys.stdin.readline()
+            if CMD.find('quit')==0:
+                sys.stdout.write("system out")
+                break
+            else:
+                sys.stdout.write("unknow command\n")
 
     finally:
         SERVER.stop()
+
+#获取excel的数据
+def getdata():
+    xl=xlrd.open_workbook(r'Master_values.xlsx')
+    table=xl.sheets()[0]
+    data=table.cell(1,1).value
+    print(data)
+    #数据切片 TX count:0X3726==>0X3726
+    cut_data=data[9:]
+    print(cut_data)
+    #数据转换 十六进制转化成十进制
+    cut_data_10=str(int(hex(cut_data).upper(),16))
+    
+    print(cut_data_10)
+    return cut_data
 
 if __name__=="__main__":
     main()
